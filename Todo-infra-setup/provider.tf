7b2faf165a050cbc29b3cdf3b164b52e3b3257eb
ShelarSantosh <shelar9356@gmail.com>
2025-12-11 19:36:35 +0530

    Update Azure subscription ID in provider configuration

--- Todo-infra-setup/provider.tf
+++ Todo-infra-setup/provider.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
   features {
 
   }
-  subscription_id = "bd3dbcd1-3262-48f5-95f6-0a9c3f3411d3"
+  subscription_id = "75c299d2-39d1-4b02-aac8-5b96bd04cb04"
   # Configuration options
 
 
